a29a8b5b93c2b2881809a8c6b5aacb2af7e6c38f
lor-man <omardamian126@gmail.com>
2021-04-14 20:15:08 -0600

    Agregado login

--- proyecto/urls.py
+++ proyecto/urls.py
@@ -13,13 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path
+from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 from proyecto import views as general
 from users import views as users
 
 urlpatterns = [
-    path('', general.homepage),
-    path('admin/', admin.site.urls),
-    path('login/',users.logon)
-]
+    path('', general.homepage, name = "homepage"),
+    path('admin/', admin.site.urls, name = "administration"),
+    path('login', users.logon, name = "logon"),
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- users/views.py
+++ users/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.contrib.auth import authenticate, login
 # Create your views here.
 def logon(request):
-    return HttpResponse("Pagina formulario de ingreso")
+    if(request.method == "POST"):
+        username = request.POST('username')
+        password = request.POST('password')
+        userSession = authenticate(request, username=username,password=password)
+        if(userSession):
+            login(request, userSession)
+            return redirect('homepage')
+        else:
+            return render(request,'index.html',{'error':"Invalid password"})
+    return render(request,"index.html")
